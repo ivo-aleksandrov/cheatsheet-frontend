4d02f3f27c9775696fa55656f7f687ce9e5483c2
Ivo Aleksandrov <71323850+ivo-aleksandrov@users.noreply.github.com>
2024-01-03 16:19:52 +0200

    Minor changes

--- src/pages/category/AddCategory.jsx
+++ src/pages/category/AddCategory.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ErrorHandler from "../../components/errorhandler/ErrorHandler";
 import "./style.css";
 
 export default function AddCategory() {
@@ -8,6 +9,8 @@ export default function AddCategory() {
     categoryName: "",
     icon: "",
   });
+
+  const [error, setError] = useState();
 
   const handleInputChange = (e) => {
     setForm({
@@ -28,6 +31,9 @@ export default function AddCategory() {
         if (response.ok) {
           navigate(`/`);
           window.location.reload(false);
+        } else {
+          console.error("Error adding category:", response.errorMessage);
+          setError(response.errorMessage);
         }
         throw new Error("Network response was not ok.");
       })
@@ -45,23 +51,52 @@ export default function AddCategory() {
         <main>
           <div className="header">
             <div className="left">
-              <h1>Add category</h1>
+              <h1>Edit category: {form.categoryName}</h1>
             </div>
           </div>
-          <div className="add-category">
-            <input
-              type="text"
-              name="categoryName"
-              onChange={handleInputChange}
-              placeholder="Enter category name"
-            />
-            <input
-              type="text"
-              name="icon"
-              onChange={handleInputChange}
-              placeholder="Enter icon"
-            />
-            <button onClick={handleSubmit}>Add Category</button>
+          <div className="bottom-data">
+            <div className="commands">
+              <div className="edit-category">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>NAME</th>
+                      <th>ICON</th>
+                      <th></th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody></tbody>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <input
+                          type="text"
+                          name="categoryName"
+                          onChange={handleInputChange}
+                          value={form.categoryName}
+                          placeholder="Enter name for category..."
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          name="icon"
+                          onChange={handleInputChange}
+                          value={form.icon}
+                          placeholder="Enter icon for category..."
+                        />
+                      </td>
+                      <td onClick={handleSubmit} className="edit-btn">
+                        <i className="bx bx-check"></i>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <ErrorHandler error={error} />
+              </div>
+            </div>
           </div>
         </main>
       </div>
