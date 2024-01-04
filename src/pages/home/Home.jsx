964c3189f589b62abcbbc14386f94d145f2d6c6d
Ivo Aleksandrov <71323850+ivo-aleksandrov@users.noreply.github.com>
2024-01-04 10:48:55 +0200

    Minor changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,6 +9,7 @@ export default function Dashboard() {
           <div className="header">
             <div className="left">
               <h1>Cheat Sheet</h1>
+              <br></br>
               <p>
                 This React App is a user-friendly and intuitive tool designed to
                 streamline the organization and management of categories and
@@ -16,50 +17,50 @@ export default function Dashboard() {
                 robust platform for users to create, modify, and delete both
                 categories and commands within those categories effortlessly.
               </p>
-
-              <h2>Key Features:</h2>
-              <ol>
+              <br></br>
+              <h3>Key Features:</h3>
+              <ul>
                 <li>
-                  <span>Category Management:</span> Users can create, rename,
+                  <b>Category Management:</b> Users can create, rename,
                   and delete categories.
                 </li>
                 <li>
-                  <span>Command Creation and Editing:</span> Add, edit, and
+                  <b>Command Creation and Editing:</b> Add, edit, and
                   delete commands within categories.
                 </li>
                 <li>
-                  <span>User-Friendly Interface: </span>Intuitive controls and
+                  <b>User-Friendly Interface: </b>Intuitive controls and
                   navigation for easy use.
                 </li>
                 <li>
-                  <span>Responsive Design: </span>Ensures usability across
+                  <b>Responsive Design: </b>Ensures usability across
                   various devices.
                 </li>
                 <li>
-                  <span>Drag-and-Drop Functionality: </span>Organize commands
+                  <b>Drag-and-Drop Functionality: </b>Organize commands
                   with ease.
                 </li>
                 <li>
-                  <span>Error Handling and Confirmation: </span>Prevent
+                  <b>Error Handling and Confirmation: </b>Prevent
                   accidental data loss.
                 </li>
                 <li>
-                  <span>Search and Filter: </span>Find specific categories or
+                  <b>Search and Filter: </b>Find specific categories or
                   commands quickly.
                 </li>
                 <li>
-                  <span>Authentication and Security: </span>Secure user data and
+                  <b>Authentication and Security: </b>Secure user data and
                   operations.
                 </li>
                 <li>
-                  <span>Undo and Redo Functionality: </span>Provides a safety
+                  <b>Undo and Redo Functionality: </b>Provides a safety
                   net for actions.
                 </li>
                 <li>
-                  <span>Export and Import: </span>Backup and transfer data
+                  <b>Export and Import: </b>Backup and transfer data
                   effortlessly.
                 </li>
-              </ol>
+              </ul>
             </div>
           </div>
         </main>
